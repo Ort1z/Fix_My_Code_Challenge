2a6ae45dc2cf54220adedae8daa3122f4da58239
MALULEKE MASINGITA OTTIS <38721983+Ort1z@users.noreply.github.com>
2024-07-24 03:36:27 +0200

    Update posts_controller.rb

--- 0x01-challenge/blog/app/controllers/posts_controller.rb
+++ 0x01-challenge/blog/app/controllers/posts_controller.rb
@@ -2,8 +2,8 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
   
   def index
-    @posts = Post.all.order('created_at DESC')
-  end
+  @posts = Post.where(online: true)
+end
 
   def new
     @post = Post.new
