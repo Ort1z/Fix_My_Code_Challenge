f71e963e87cec3e42b8cb2b71b33f9132773f773
MALULEKE MASINGITA OTTIS <38721983+Ort1z@users.noreply.github.com>
2024-07-24 03:43:25 +0200

    Update post.rb

--- 0x01-challenge/blog/app/models/post.rb
+++ 0x01-challenge/blog/app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ApplicationRecord
   has_many  :comments, dependent: :destroy
   validates :title, presence: true, length: {minimum: 5}
 	validates :body,  presence: true
+
+  attribute :online, :boolean, default: true
 end
